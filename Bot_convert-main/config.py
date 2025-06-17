8a9edf957088b50360355161d9874b17ad80d8d5
SaitBakhteev <148718664+SaitBakhteev@users.noreply.github.com>
2025-06-17 23:09:41 +0300

    Delete Bot_convert-main directory

--- Bot_convert-main/config.py
+++
@@ -1,7 +0,0 @@
-TOKEN='6967280071:AAGMm00Qft6nrQbPuK1DZqN2c5OVEZ8AwGo'
-AVAILABLE_UNITS = {'доллар': 'USD',  # словарь доступных валют
-                       'рубль': 'RUB',
-                       'йена': 'JPY',
-                       'биткоин': 'BTC',
-                        'евро':'EUR',
-                   'юань':'CNY'}
